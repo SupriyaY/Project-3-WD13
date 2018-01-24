88d2746a7aaa710870a42f960a6c79b3083d433c
Supriya <supriya.yerramilli@gmail.com>
2018-01-24 14:47:21 -0500

    css on first page - buttons

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,22 +1,34 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import UserPage from './UserPage'
+import styled from 'styled-components'
+
+const Title = styled.h1 `
+  font-size: 3.5em;
+  text-align: center;
+  color: turquoise;
+`;
+
+// const Content = styled.div` width: 100vw height:
 
 class Home extends Component {
 
     render() {
+
         return (
             <div>
-<h1>RoomStyle</h1>
-
+            <div>
+                <Title>RoomStyle</Title>
+                </div>
 <div>
-  <Link to="/users">Users</Link>  
+                 <button><Link to="/users">Users</Link></button>
+                   <button><Link to="#">Rooms</Link></button>
+                   <button><Link to="#">Funishings</Link></button>
+                    </div>
 </div>
-</div>
-
         )
     }
-
 }
+
 
 export default Home;
