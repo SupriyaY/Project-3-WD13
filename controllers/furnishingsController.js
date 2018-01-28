1374b0a56eee533717296926a469a1c20dea4c7b
Supriya <supriya.yerramilli@gmail.com>
2018-01-28 15:35:35 -0500

    creating routes to show furnishings

--- controllers/furnishingsController.js
+++ controllers/furnishingsController.js
@@ -3,28 +3,69 @@ const router = express.Router()({mergeParams: true})
 const User = require('../db/models/User')
 
 
+//Index
+router.get('/', async (request, response) => {
+try{
+const room = await Room.findbyId(request.params.roomId)
+const showFurnishing = room.furnishing
+response.json(showFurnishing)
+} catch (err) {
+console.log(err)
+}
+})
 
-// router.get('/:activityId', (request, response) => {
-//     const userId = request.params.userId
-//     const roomId = request.params.cityId
-//     const activityId = request.params.activityId
 
-//     User
-//         .findById(userId)
-//         .then((user) => {
-//             const room = user
-//                 .room
-//                 .id(roomId)
-//             const furnishing = room
-//                 .funishingsToAdd
-//                 .id(activityId)
 
-//             response.render('activities/show', {userId, city, activity, pageTitle: 'Activities'})
-//         })
-//         .catch((error) => {
-//             console.log(error)
-//         })
-// })
+
+
+
+
+router.get('/', (request, response) => {
+    const userId = request.params.userId
+
+    User
+        .findById(userId)
+        .then((user) => {
+            response.render('cities/index', {
+                userName: `${user.name}`,
+                userId: user._id,
+                cities: user.cities,
+                pageTitle: 'Cities'
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+
+
+
+
+
+
+
+router.get('/:Id', (request, response) => {
+    const userId = request.params.userId
+    const roomId = request.params.cityId
+    const activityId = request.params.activityId
+
+    User
+        .findById(userId)
+        .then((user) => {
+            const room = user
+                .room
+                .id(roomId)
+            const furnishing = room
+                .funishingsToAdd
+                .id(activityId)
+
+            response.render('activities/show', {userId, city, activity, pageTitle: 'Activities'})
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
 
 
 
--- controllers/roomsController.js
+++ controllers/roomsController.js
@@ -2,43 +2,26 @@ const express = require('express')
 const router = express.Router()({mergeParams: true})
 const User = require('../db/models/User')
 
-
-//INDEX
-router.get('/', (request, response) => {
-    const userId = request.params.userId
-    User.findById(userId)
-        .then((user) => {
-            response.json(user)
-            })
-        .catch((error) => {
-            console.log(error)
-        })
+//INDEX page for all of the cities
+router.get('/', async(request, response) => {
+    try {
+        const userId = request.params.userId
+        const user = await User.findById({})
+        response.json(rooms)
+    } catch (err) {
+        console.log(err)
+    }
 })
-
-
-
 
 //SHOW
-router.get('/:roomId', (request, response) => {
-    const roomId = request.params.roomId
-
-    User
-        .findById(userId)
-        .then((user) => {
-            const room = user
-                .rooms
-                .id(roomId)
-            response.json(userId, room)
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+router.get('/:roomId', async(request, response) => {
+    try {
+        const roomId = request.params.roomId
+        const room = await Room.findById(request.params.roomId)
+        response.json(room)
+    } catch (err) {
+        response.send(err)
+    }
 })
-
-
-
-
-
-
 
 module.exports = router
